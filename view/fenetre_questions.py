78142b550e6e9fee646187ebdadf8666e4b12db6
asarrafeha <asarrafeha@2D-INSA-001.local>
2025-04-17 09:59:07 +0200

    création de la fenetre pour afficher les questions

---
+++ view/fenetre_questions.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tkinter as tk
+from tkinter import *
+
+
+class VueQuestions(tk.Tk) : 
+    
+    def __init__(self, question) : #question est un dico avec { 'question' : XXX, 'choix' : 'unique'/ 'multiple', 'options':[option1, option2, option3, option4] }
+        super().__init__()
+        self.question = question
+        
+        self.geometry("450x300")
+        self.title("Pour en apprendre plus sur vous")
+        
+        self.text = tk.Label(self, text = question['question'])
+        self.text.pack(side=tk.TOP) #la question est en haut de la fenêtre
+        
+        #à quel point la question est importante pour l'utilisateur
+        self.importance = tk.Label(self, text = "Importance de cette question selon vous :")
+        self.importance.pack(side=tk.TOP)
+        self.sc1 = tk.Scale(self, from_ = 1, to = 10, orient = 'horizontal')
+        self.sc1.pack(side=tk.TOP)
+        
+        #options de réponses
+        self.frame = tk.Frame(self)
+        self.frame.pack(side=tk.TOP)
+        
+        if self.question['choix'] == 'multiple' : 
+            self.choix1 = tk.BoolVar()
+            self.choix1.set(False)
+            self.choix2 = tk.BoolVar()
+            self.choix2.set(False)
+            self.choix3 = tk.BoolVar()
+            self.choix3.set(False)
+            self.choix4 = tk.BoolVar()
+            self.choix4.set(False)
+            self.option1 = tk.Checkbutton(self.frame, text = self.question['option'][0], variable = self.choix1, value = True)
+            self.option1.grid(row=1, column=0)
+            self.option2 = tk.Checkbutton(self.frame, text = self.question['option'][1], variable = self.choix2, value = True)
+            self.option2.grid(row=1, column=0)
+            self.option3 = tk.Checkbutton(self.frame, text = self.question['option'][2], variable = self.choix3, value = True)
+            self.option3.grid(row=1, column=0)
+            self.option4 = tk.Checkbutton(self.frame, text = self.question['option'][3], variable = self.choix4, value = True)
+            self.option4.grid(row=1, column=0)
+        if question['choix'] == 'unique' : 
+            self.choix = tk.BoolVar()
+            self.choix.set(False)
+            self.option1 = tk.Radiobutton(self.frame, text = self.question['option'][0], variable = self.choix, value = True)
+            self.option1.grid(row=1, column=0)
+            self.option2 = tk.Radiobutton(self.frame, text = self.question['option'][1], variable = self.choix, value = True)
+            self.option2.grid(row=1, column=0)
+            self.option3 = tk.Radiobutton(self.frame, text = self.question['option'][2], variable = self.choix, value = True)
+            self.option3.grid(row=1, column=0)
+            self.option4 = tk.Radiobutton(self.frame, text = self.question['option'][3], variable = self.choix, value = True)
+            self.option4.grid(row=1, column=0)
+        
+        
+        #boutons suivant et précédent
+        self.suiv = tk.Button(self, text = 'suivant')
+        self.suiv.pack(side=tk.BOTTOM)
+        self.prec = tk.Button(self, text = 'précédent')
+        self.prec.pack(side=tk.BOTTOM)
+
+question = { 'question' : 'XXX', 'choix' : 'unique', 'options':['option1', 'option2', 'option3', 'option4'] }
+app = VueQuestions(question)
+app.mainloop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
