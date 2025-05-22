79854b494ee2f7696b578ba48b57af7a7ddc770b
mprivitera <matias.privitera@insa-lyon.fr>
2025-05-22 10:01:22 +0200

    debut score flou appariment

--- controller/appariement.py
+++ controller/appariement.py
@@ -22,22 +22,30 @@ class People:
 
         self.n_perso=math.sqrt(self.n_perso)
         self.n_attentes=math.sqrt(self.n_attentes)
-       ## i love martin print(self.perso_pond "\n",self.attente_pond "\n",self.n_perso "\n",self.n_attentes)
+        print(self.perso_pond ,self.attente_pond ,self.n_perso ,self.n_attentes)
 
-    def produit_vect(self,u,v):
+    def produit_scal(self,u,v):
         val=0
         for i in range (len(u)):
             val+= u[i]*v[i]
         return(val)
 
-    def similarite (self,personne2):
-        simAB= self.produit_vect(self.attente_pond,personne2.perso_pond)/(self.n_attentes*personne2.n_perso)
-        simBA=self.produit_vect(personne2.attente_pond,self.perso_pond)/(personne2.n_attentes*self.n_perso)
+    def similarite_cos (self,personne2):
+        simAB= self.produit_scal(self.attente_pond,personne2.perso_pond)/(self.n_attentes*personne2.n_perso)
+        simBA=self.produit_scal(personne2.attente_pond,self.perso_pond)/(personne2.n_attentes*self.n_perso)
         comp= ((simAB+simBA)/2)*100
         return comp
-    
+"""
+    def score_flou (self,personne2):
+        score_1=0
+
+        for i in range len(self.vect_perso):
+            val= math.exp((personne2.vect_attentes[i]-self.vect_attentes[i])*coeff[i]*self.vect_importances)
+"""
+##test: 
+
 Martin=People([1,2,5,3,8],[4,5,2,2,6],[4,2,2,6,5])
 Davi=People([8,1,1,4,3],[5,2,5,4,3],[2,5,5,2,3])
-Matias=People([4,5,2,2,6],[1,2,5,3,8],[4,2000,2000,600,500])
+Matias=People([4,5,2,2,6],[1,2,5,3,8],[40,20,20,60,50])
 
 print(Martin.similarite(Matias))
