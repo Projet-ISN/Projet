1a64faf3e5a9c135eaec8256eb0828873fb9a8a5
martintobong <martin.tobon-guevara@insa-lyon.fr>
2025-06-02 20:12:07 +0200

    Vue principale terminee

--- view/vue_principale.py
+++ view/vue_principale.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
-from creation_compte import Creation_compte #À corriger avec le nom du fichier correct
+#from AccountCreationView import AccountCreationView
 
 class VuePrincipale(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry("450x300")
+        #self.geometry("450x300")
         self.title("INTERFACE DE CONNEXION")
         
         
@@ -19,30 +19,27 @@ class VuePrincipale(tk.Tk):
         self.password_perdu = tk.Button(self, text='Mot de passe oublié ?')
         self.new_user = tk.Button(self, text='Créer un nouveau compte')
         
-        self.user_label.grid(row=1, column=1)
-        self.user_entry.grid(row=1, column=2)
-        self.password_label.grid(row=2, column=1)
-        self.password_entry.grid(row=2, column=2)
+        self.user_label.grid(row=1, column=2)
+        self.user_entry.grid(row=1, column=3)
+        self.password_label.grid(row=2, column=2)
+        self.password_entry.grid(row=2, column=3)
         
-        self.validate_button.grid(row=3, column=1, columnspan=2)
-        self.password_perdu.grid(row=4, column=1)
-        self.new_user.grid(row=4,column=4)
+        self.validate_button.grid(row=4, column=2, columnspan=2)
+        self.password_perdu.grid(row=6, column=1)
+        self.new_user.grid(row=6,column=4)
         
         self.password_perdu.bind('<Button-1>', self.mot_de_passe_perdu)
-        self.new_user.bind('<Button-1>', self.creer_nouveau_compte)
-        
-        
-    #def valider_utilisateur(self, event):
-        
+        #self.new_user.bind('<Button-1>', self.creer_nouveau_compte)
         
         
     def mot_de_passe_perdu(self,event):
         messagebox.showinfo(message="Dommage :(")
         
         
-    def creer_nouveau_compte(self, event):
-        self.destroy()
-        nouvelle_interface = Creation_compte(self)
+    #def creer_nouveau_compte(self, event):
+        #self.destroy()
+        #nouvelle_interface = AccountCreationView(self)
+        #nouvelle_interface.grab_set()
         
         
 
